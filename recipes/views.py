9252427909ba0d804baa74f7549c83668a38a593
Lucas Palmeira <lucaspalmeira0202@gmail.com>
2025-12-04 05:37:00 -0300

    Templates

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "recipes/home.html")
+
+def sobre(request):
+    return HttpResponse("Sobre")
+
+def contato(request):
+    return HttpResponse("Contato")
+# Create your views here.
