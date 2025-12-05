7abdbc4fae4b00978954888ad9b518fb45e6b4c4
Lucas Palmeira <lucaspalmeira0202@gmail.com>
2025-12-04 23:57:00 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "recipes/home.html")
+    return render(request, "recipes/home.html", context={
+        "name": "Lucas Palmeira",
+    })
 
-def sobre(request):
-    return HttpResponse("Sobre")
-
-def contato(request):
-    return HttpResponse("Contato")
-# Create your views here.
