61f90a5361902ba2054313c1beaa24100e2fa710
Lucas Palmeira <lucaspalmeira0202@gmail.com>
2025-12-10 05:53:34 -0300

    Add models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        'categories.Category', on_delete=models.SET_NULL, null=True, blank=True, related_name='recipes'
+    )
+    author = models.ForeignKey(
+        'authors.Author', on_delete=models.SET_NULL, null=True, blank=True, related_name='recipes'
+    )
+
+    def __str__(self):
+        return self.title
+
+
+
+# Title description slug 
+# preparation_time preparation_time_unit
+# servings servings_unit
+# preparation_steps
+# preparation_steps_is_html
+# created_at updated_at
+# is_published
+# cover
+# category (Relação)
+# Author (relação)
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, "recipes/pages/home.html", context={
-        "name": "Lucas Palmeira",
+        'recipes':[make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, "recipes/pages/recipe-view.html", context={
-        "name": "Lucas Palmeira",
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
