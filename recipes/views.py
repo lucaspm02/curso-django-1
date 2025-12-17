9769d7ac12eccd6c331c6820f5803a32e5c8dbe0
Lucas Palmeira <lucaspalmeira0202@gmail.com>
2025-12-17 03:51:06 -0300

    Configurando 404_not Found

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -8,14 +9,21 @@ def home(request):
     ).order_by('-id')
     return render(request, "recipes/pages/home.html", context={
         'recipes': recipes,
+      
     })
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, 
         is_published=True).order_by('-id')
+    
+    if not recipes:
+        raise Http404("Not Found ☻")
+
+
     return render(request, "recipes/pages/category.html", context={
         'recipes': recipes,
+          'title': f'{recipes.first().category.name} - Category |'
     })
 
 def recipe(request, id):
